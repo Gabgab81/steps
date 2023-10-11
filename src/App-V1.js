5ebf567feaaf88eb5715849a9a7c22bb3d52c373
gabriel roux <rouxgab81@gmail.com>
2023-10-11 14:45:07 -0400

    add children props

--- src/App-V1.js
+++ src/App-V1.js
@@ -10,7 +10,7 @@ const App = () => {
   return <div>
     <Steps />
     {/* <Steps /> */}
-    {/* <Challengeone /> */}
+    <Challengeone />
   </div>
 }
 
@@ -31,8 +31,15 @@ const Steps = () => {
     // if (step < 3) setStep(step + 1);
     if (step < 3) setStep(s => s + 1);
 
+    //Work but bad practice
+    // test.name = "Alfred"
+    //Good Practice
+    // setTest({name: "Alfred"})
   }
 
+  // const handleClose = () => {
+  //   setIsOpen(!isOpen);
+  // }
 
   return (
 
@@ -55,23 +62,24 @@ const Steps = () => {
             <div className={`${step > 2 && "active"}`}>3</div>
           </div>
 
-          <StepMessage step={step}>
-            {messages[step - 1]}
-          </StepMessage> 
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
 
           <div className="buttons">
-            <Button 
-              bgColor='#7950f2' 
-              textColor= "#fff" 
+            <button 
+              style={{backgroundColor: '#7950f2', color: "#fff"}}
               onClick={handlePrevious}
-            ><span>"👈"</span> Previous
-            </Button>
-            <Button 
-              bgColor='#7950f2' 
-              textColor= "#fff" 
+            >
+              Previous
+            </button>
+            <button 
+              style={{backgroundColor: '#7950f2', color: "#fff"}}
               onClick={handleNext}
-            >Next<span>'👉'</span>
-            </Button>
+            >
+              Next
+            </button>
           </div>
 
         </div>)
@@ -79,30 +87,6 @@ const Steps = () => {
     </div>
   )
 }
-
-const StepMessage = ({step, children}) => {
-  return (
-    <div className="message">
-      <h3>Step {step}: </h3>
-      {children}
-    </div>
-  )
-}
-
-const Button = ({textColor, bgColor, onClick, children}) => {
-  return (
-    <button 
-      style={{backgroundColor: bgColor, color: textColor}}
-      onClick={onClick}
-    >
-      {/* <span>{emoji}</span> {text} */}
-      {children}
-    </button>
-  )
-}
-
-
-//Challenge One
 
 const Challengeone = () => {
   return (
